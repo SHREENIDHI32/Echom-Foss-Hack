0caedbda5c90b437453c44439d3a820cdd2ae18a
Snegakutty <132530737+Snegakutty@users.noreply.github.com>
2025-02-23 20:17:50 +0530

    feature

--- backend/config/db.js
+++
@@ -1,6 +0,0 @@
-import mongoose from "mongoose";
-
-export const  connectDB = async () =>{
-    await mongoose.connect('mongodb+srv://snegaofficialk:snegaofficialk@cluster0.45cf8.mongodb.net/Echom').then(()=>console.log("DB Connected"))
-}
-
